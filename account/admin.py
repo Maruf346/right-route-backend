33ca5d403d92c7f0f17e87f772518894c792c87f
samimosmansabuj <samim.o.sabuj01@gmail.com>
2026-06-29 12:56:21 +0600

    device model register in admin panel!!!!

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, OTPVerification, Team, TeamMember, TeamMemberInvite, UserPaymentMethod
+from .models import User, OTPVerification, Team, TeamMember, TeamMemberInvite, UserPaymentMethod, UserLogDevice
 
 admin.site.register(User)
 admin.site.register(OTPVerification)
@@ -7,3 +7,4 @@ admin.site.register(Team)
 admin.site.register(TeamMember)
 admin.site.register(TeamMemberInvite)
 admin.site.register(UserPaymentMethod)
+admin.site.register(UserLogDevice)
